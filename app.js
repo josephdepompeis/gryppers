26e1b778e7d500b03d33ba964bd4b0fa2fead8a7
Joseph DePompeis <josephdepompeis@gmail.com>
2016-05-03 23:40:26 -0400

    access data based on input

--- app.js
+++ app.js
@@ -44,23 +44,19 @@ sampleApp.controller('ShowOrdersController', function($scope) {
 sampleApp.controller('JoeOrdersController', function($scope, $http) {
 
 	$scope.message = 'This is JOE orders screen';
-  //
-  // $http({
-  //     method: 'GET',
-  //     url: 'http://localhost:3001/characters/1.json'
-  // }).then(function(response){
-  //     console.log(response);
-  // })
+  $http.get('http://localhost:3001/characters/1.json').then(function success(response){
+        console.log(response);
+        $scope.character_one = response;
+    }, function error(response){
+      console.log('GET failed in appController');
+  });
 
-  //check and see if user is logged in...if they are, show a user icon in the header that is a link to the edit-profile page
-  // if($scope.logged === true){
-    $http.get('http://localhost:3001/characters/1.json').then(function success(response){
-          console.log(response);
-        	$scope.message = response;  
-      }, function error(response){
-        console.log('GET failed in appController');
-    });
-  // };
+  $http.get('http://localhost:3001/characters/2.json').then(function success(response){
+        console.log(response);
+        $scope.character_two = response;
+    }, function error(response){
+      console.log('GET failed in appController');
+  });
 
 
 
